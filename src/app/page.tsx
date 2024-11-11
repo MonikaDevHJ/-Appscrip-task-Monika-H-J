607d6dbebb0e52c5af710134eddbf1249f3cc73a
Monika H J <monikahj7@gmail.com>
2024-11-11 11:00:14 +0530

    Apsscript Project is done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import { useState } from "react";
-import Image from "next/image";
-import Link from "next/link";
+
 import "./styles.css";
 import Navbar from "../components/Navbar/Navbar";
 import Main from "../components/main/Main"
